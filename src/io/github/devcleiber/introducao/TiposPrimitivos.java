d348a223f18591ce40f7cd9a22a8c86d8c96b85f
Cleiber Dorneles <cleiberbomfim021@gmail.com>
2026-07-15 18:55:26 -0300

    First Commit!

---
+++ src/io/github/devcleiber/introducao/TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package io.github.devcleiber.introducao;
+// Convenções de Variáveis
+public class TiposPrimitivos  {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int idade= 19;
+        S
+    }
+}
+
