b4b654159e20e84e8142f65d82cb8711ce960336
Cleiber Dorneles <cleiberbomfim021@gmail.com>
2026-08-20 20:09:03 -0300

    second exercise today

---
+++ Estrutura Sequencial Básica/circulo.java
@@ -0,0 +1,12 @@
+import util.java.Scanner;
+
+public class circulo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double raio, area;
+        System.out.print("Digite o valor do raio: ");
+        raio = sc.nextDouble();
+        area = Math.PI * Math.pow(raio, 2);
+        System.out.println("A área do círculo é: " + area);
+    }
+}
