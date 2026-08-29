fb35a9ab79b69cf54253ba7abd4cf6df65dc62f1
Cleiber Dorneles <cleiberbomfim021@gmail.com>
2026-08-29 14:35:11 -0300

    Little Exercise and learning loops.

---
+++ quadrantes.java
@@ -0,0 +1,31 @@
+//Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence (Q1, Q2, Q3 ou Q4). O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma). //
+
+import java.util.Scanner;
+
+public class quadrantes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite as coordenadas X e Y: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != 0 && y != 0) {
+            if (x > 0 && y > 0) {
+                System.out.println("Q1");
+            } else if (x < 0 && y > 0) {
+                System.out.println("Q2");
+            } else if (x < 0 && y < 0) {
+                System.out.println("Q3");
+            } else if (x > 0 && y < 0) {
+                System.out.println("Q4");
+            }
+
+            System.out.println("Digite as coordenadas X e Y: ");
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
