984d6e6f019451374f823967e173b57cd2a164c8
Cleiber Dorneles <cleiberbomfim021@gmail.com>
2026-08-31 10:03:36 -0300

    Add Loop Exercises Yesterday
    Add some loop exercises I forgot to commit yesterday

---
+++ Estruturas de Repetição/SomaImpar.java
@@ -0,0 +1,9 @@
+//Leia 2 valores inteiros X e Y (em qualquer ordem). A seguir, calcule e mostre a soma dos números impares entre eles.//
+
+import java.util.Scanner;
+
+public class SomaImpar {
+    public static void main(String[] args) {
+
+    }
+}
