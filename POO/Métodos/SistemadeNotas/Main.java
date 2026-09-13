7007f7f2d09ae07432b9fb1b70e4625391c9ea18
Cleiber Dorneles <cleiberbomfim021@gmail.com>
2026-09-13 20:56:10 -0300

    Update

--- POO/Métodos/SistemadeNotas/Main.java
+++ POO/Métodos/SistemadeNotas/Main.java
@@ -5,9 +5,11 @@ import domain.MediaAluno;
 public class Main {
     public static void main(String[] args) {
         MediaAluno Alunotest = new MediaAluno();
-        Alunotest.setNome("Cleiber");
-        double media = Alunotest.calcularMedia(8.5, 7.0, 9.0);
-        System.out.println("Média do aluno: " + media);
-
+        Alunotest.setNome("Cleiber"); //exibe nome do aluno
+        double media = Alunotest.calcularMedia(8.5, 7.0, 9.0); //calcula a média das notas
+        Alunotest.exibirMedia(media); //exibe a média do aluno
+        Alunotest.exibirsituacao(media); //exibe a situação do aluno (aprovado ou reprovado)
+        
+        
     }
 }
