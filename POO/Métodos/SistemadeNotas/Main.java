1b6f45790a32d8c28a5cba38655f3e63ef10ff9d
Cleiber Dorneles <cleiberbomfim021@gmail.com>
2026-09-15 16:01:46 -0300

    Edit file

--- POO/Métodos/SistemadeNotas/Main.java
+++ POO/Métodos/SistemadeNotas/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Alunotest.setNome("Cleiber"); //exibe nome do aluno
         double media = Alunotest.calcularMedia(8.5, 7.0, 9.0); //calcula a média das notas
         Alunotest.exibirMedia(media); //exibe a média do aluno
-        Alunotest.exibirsituacao(media); //exibe a situação do aluno (aprovado ou reprovado)
+        Alunotest.exibirSituacao(media); //exibe a situação do aluno (aprovado ou reprovado)
         
         
     }
